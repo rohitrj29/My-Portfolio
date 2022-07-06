6dff8594dbff8566caa9d02c4163e49cd91654ce
rohitrj29 <f20200906@pilani.bits-pilani.ac.in>
2022-07-06 17:20:01 +0530

    header complete

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -6,19 +6,20 @@ import HeaderSocials from './HeaderSocials'
 
 function Header() {
   return (
-    <header >
+    <header>
     <div className="container header__container">
     <h5>Hello I'm</h5>
     <h1>Rohit Raj</h1>
-    <h5 className='text-light'>Fullstack Developer | Designer</h5>
+    <h5 className='text-light'>Fullstack Developer | UI/UX Designer</h5>
     <CTA />
     
-    </div>
+   
     <HeaderSocials/>
     <div className='me'>
-      <img src={ME} alt=''/>
+      <img src={ME} alt='me'/>
     </div>
     <a href='#contact' className='scroll__down'>Scroll Down</a>
+    </div>
     </header>
   )
 }
