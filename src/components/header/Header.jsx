2200046d2004cf332d6439dcd35f43ffff6061d2
rohitrj29 <f20200906@pilani.bits-pilani.ac.in>
2022-07-06 15:49:59 +0530

    header content

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,8 +1,24 @@
 import React from 'react'
 import './Header.css'
+import CTA from './CTA'
+import ME from '../../assets/me.png'
+import HeaderSocials from './HeaderSocials'
+
 function Header() {
   return (
-    <div>Header</div>
+    <header >
+    <div className="container header__container">
+    <h5>Hello I'm</h5>
+    <h1>Rohit Raj</h1>
+    <h5 className='text-light'>Fullstack Developer | Designer</h5>
+    <CTA />
+    <HeaderSocials/>
+    </div>
+    <div className='me'>
+      <img src={ME} alt=''/>
+    </div>
+    <a href='#contact' className='scroll__down'>Scroll Down</a>
+    </header>
   )
 }
 
