4d47ca901f649a778782dd23c3e5e5fdd91a196f
rohitrj29 <f20200906@pilani.bits-pilani.ac.in>
2022-07-09 23:29:10 +0530

    serv jsx

--- src/components/services/Services.jsx
+++ src/components/services/Services.jsx
@@ -1,8 +1,43 @@
 import React from 'react'
 import './Services.css'
+import {BiCheck} from "react-icons/bi"
+0
 function Services() {
   return (
-    <section id='services'>service</section>
+    <section id='services'>
+      <h5>What I Offer</h5>
+      <h2>Services</h2>
+      <div className='container services__container'>
+        <article className='service'>
+          <div className='service__head'>
+            <h3>UI/UX Design</h3>
+
+          </div>
+          <ul className='service__list'>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Non nostrud consectetur proident aute mollit amet qui.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Non nostrud consectetur proident aute mollit amet qui.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Non nostrud consectetur proident aute mollit amet qui.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Non nostrud consectetur proident aute mollit amet qui.</p>
+            </li>
+            <li>
+              <BiCheck className='service__list-icon'/>
+              <p>Non nostrud consectetur proident aute mollit amet qui.</p>
+            </li>
+          </ul>
+        </article>
+      </div>
+    </section>
   )
 }
 
