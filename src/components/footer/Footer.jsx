71269ac77060fb9fe6361bda6ec75c51a9e2ed58
rohitrj29 <f20200906@pilani.bits-pilani.ac.in>
2022-07-12 01:32:04 +0530

    footer jsx

--- src/components/footer/Footer.jsx
+++ src/components/footer/Footer.jsx
@@ -1,8 +1,30 @@
 import React from 'react'
 import './Footer.css'
+import {FaFacebookF} from 'react-icons/fa'
+import {FiInstagram} from 'react-icons/fi'
+import {IoLogoTwitter} from 'react-icons/io'
+
 function Footer() {
   return (
-    <section id='contact'>contact</section>
+    <footer>
+      <a href='#' className='footer__logo'>Rohit</a>
+      <ul className='permalinks'>
+        <li><a href='#'>Home</a></li>
+        <li><a href='#about'>About</a></li>      
+        <li><a href='#services'>Services</a></li>
+        <li><a href='#experience'>Experience</a></li>
+        <li><a href='#portfolio'>Portfolio</a></li>
+        <li><a href='#contact'>Contact</a></li>
+      </ul>
+      <div className='footer__socials'>
+        <a href='hhtps://facebook.com'><FaFacebookF/></a>
+        <a href='hhtps://instagram.com'><FiInstagram/></a>
+        <a href='hhtps://twitter.com'><IoLogoTwitter/></a>
+      </div>
+      <div className='footer__copyright'>
+        <small>&copy;All rights reserved.</small>
+      </div>
+    </footer>
   )
 }
 
